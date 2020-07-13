6128adf06da5887e43565428c264b836f9a837ba
zloeffle <40178843+zloeffle@users.noreply.github.com>
2020-07-13 12:52:57 -0500

    feature engineering

--- predict.py
+++ predict.py
@@ -32,10 +32,10 @@ class Trader:
             end_price = data.loc[end_date,'Adj Close']
 
             # features
-            df.loc[t,'14 Day SMA'] = simple_moving_average(data,14).loc[end_date,]
-            df.loc[t,'28 Day SMA'] = simple_moving_average(data,28).loc[end_date,]
-            df.loc[t,'14 Day EMA'] = exponential_moving_average(data,14).loc[end_date,]
-            df.loc[t,'28 Day EMA'] = exponential_moving_average(data,28).loc[end_date,]
+            #df.loc[t,'14 Day SMA'] = simple_moving_average(data,14).loc[end_date,]
+            #df.loc[t,'28 Day SMA'] = simple_moving_average(data,28).loc[end_date,]
+            #df.loc[t,'14 Day EMA'] = exponential_moving_average(data,14).loc[end_date,]
+            #df.loc[t,'28 Day EMA'] = exponential_moving_average(data,28).loc[end_date,]
             df.loc[t,'RSI'] = relative_strength_index(data)
             df.loc[t,'MFI'] = money_flow_index(data)
             #df.loc[t,'Start Price'] = start_price
@@ -53,28 +53,4 @@ if __name__ == '__main__':
     test1 = pd.read_csv('test1.csv',index_col='Ticker')
     test2 = pd.read_csv('test2.csv',index_col='Ticker')
     test3 = pd.read_csv('test3.csv',index_col='Ticker')
-
-    cols = list(train.columns)
-    model = DecisionTree(12)
-    root = model.build_tree(train,5)
     
-    
-    for t in train.index:
-        pred = model.predict(root,train.loc[t,:cols[-1]])
-        train.loc[t,'Prediction'] = pred
-    print(train)
-
-    for t in test1.index:
-        pred = model.predict(root,test1.loc[t,:cols[-1]])
-        test1.loc[t,'Prediction'] = pred
-    print(test1)
-
-    for t in test2.index:
-        pred = model.predict(root,test2.loc[t,:cols[-1]])
-        test2.loc[t,'Prediction'] = pred
-    print(test2)
-
-    for t in test3.index:
-        pred = model.predict(root,test3.loc[t,:cols[-1]])
-        test3.loc[t,'Prediction'] = pred
-    print(test3)
