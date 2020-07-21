7d63c738d6c7d9490a5da8939d950ccf28f67768
zloeffle <zloeffle@iu.edu>
2020-07-21 13:49:43 -0500

    revising backtesting strategy

--- trade.py
+++ trade.py
@@ -45,62 +45,7 @@ class Trader:
         # add adjusted closing prices for our date range to result dataframe
         results['ADJ CLOSE'] = temp['Adj Close']
         results = results.round(2)
-        return data,results
-
-    def backtest(self,stock,start,end):
-        portfolio = {}
-
-        # iterate through input stocks
-        data,results = self.generate_features(stock,start,end)       
-    
-        # iterate rows in result dataframe
-        for row in results.index: 
-            row = row.strftime('%Y-%m-%d')
-            profit = 0
-
-            # check for buy signal
-            if results.loc[row,'SIGNAL'] == 1:
-                print(stock)
-                print(portfolio)
-                print(results.loc[:row,:])
-                shares = int(input('Enter shares to buy: '))
-                
-                if not stock in portfolio:           
-                    portfolio[stock] = [(results.loc[row,'ADJ CLOSE'],shares)]   
-                    action = 'Buying %d shares of %s' % (shares,stock)              
-                else:
-                    if shares == 0:
-                        action = 'Not buying more shares of %s' % (stock)
-                    else:
-                        portfolio[stock].append((results.loc[row,'ADJ CLOSE'],shares))
-                        action = 'Buying %d more shares of %s' % (shares,stock)
-
-            # check for sell signal
-            elif results.loc[row,'SIGNAL'] == -1:
-                if stock in portfolio:
-                    sell_price = results.loc[row,'ADJ CLOSE']
-                    print(s)
-                    print(portfolio)
-                    print(results.loc[:row,:])
-                    shares = int(input('Enter shares to sell: '))
-                    if shares == 0:
-                        print('Not selling anymore shares of %s' % (stock))
-                    else:
-                        tot_shares = 0
-                        for item in portfolio[stock]:
-                            tot_shares += item[1]
-                            profit += (item[0] * item[1])
-                        profit = (sell_price * tot_shares) - profit
-                        action = 'Sold %s for profit of %.2f' % (stock,profit)
-                        del portfolio[stock]
-                else:
-                    action = '%s not owned so cannot sell' % (stock)
-            else:
-                action = 'HOLD'
-            
-            results.loc[row,'ACTION'] = action
-            results.loc[row,'PROFIT'] = profit
-        return results,portfolio
+        return results
 
 if __name__ == '__main__':
     t = Trader(500)
@@ -109,7 +54,9 @@ if __name__ == '__main__':
     start = '2020-05-01'
     end = '2020-05-29'
     for s in tickers:
-        res,port = t.backtest(s,start,end)
+        res = t.generate_features(s,start,end)
+        print(s)
         print(res)
-        print(port)
+        print()
+        
     
