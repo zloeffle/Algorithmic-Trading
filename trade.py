b805776bc36b5a3a39c33764f822d9f96db739f9
zloeffle <zloeffler22@gmail.com>
2020-09-12 17:57:47 -0500

    update

--- trade.py
+++ trade.py
@@ -49,12 +49,72 @@ class Trader:
         results['DATE'] = results.index
         return results
 
+    def get_stocks(self,stocks,start,end):
+        to_buy = {}
+        to_sell = {}
+
+        for stock in stocks:
+            data = self.generate_features(stock,start,end)
+            if data is not None:
+                for date in data.index:
+                    rsi = data.loc[date,'RSI-SIGNAL']
+                    bb = data.loc[date,'BB-SIGNAL']
+
+                    # BUY
+                    if rsi == 1 and bb == 1:
+                        if stock not in to_buy:
+                            date = date.to_pydatetime().strftime('%Y-%m-%d')
+                            to_buy[stock] = {'date':date,'price':data.loc[date,'PRICE'],'rsi':data.loc[date,'RSI']}
+                    # SELL
+                    if rsi == -1 and bb == -1:
+                        if stock in to_buy:
+                            date = date.to_pydatetime().strftime('%Y-%m-%d')
+                            to_sell[stock] = {'date':date,'price':data.loc[date,'PRICE'],'rsi':data.loc[date,'RSI']}
+                            
+        return to_buy,to_sell
+
+    def simulate(self,stocks,start,end,cash):
+        portfolio = {}
+        portfolio_value = 0
+        total_value = 0
+        trade_history = pd.DataFrame(columns=['DATE','TICKER','PRICE','RSI','ACTION','PROFIT'])
+        to_buy,to_sell = self.get_stocks(stocks,start,end)
+
+        i = 0
+        for stock in to_buy:
+            data = to_buy[stock]
+            portfolio[stock] = data
+            cash -= data['price']
+            portfolio_value += data['price']
+            trade_history.loc[i,:] = [data['date'],stock,data['price'],data['rsi'],'BUY',0]                
+            i += 1
+        
+        for stock in to_sell:
+            data = to_sell[stock]
+            portfolio_value -= portfolio[stock]['price']
+            portfolio.pop(stock)
+            cash += data['price']
+            profit = data['price']-to_buy[stock]['price']
+            trade_history.loc[i,:] = [data['date'],stock,data['price'],data['rsi'],'SELL',profit]
+            i += 1
+
+        portfolio_value = round(portfolio_value,2)
+        total_value = portfolio_value + cash
+        total_profit = trade_history['PROFIT'].sum()
+        print(trade_history)
+        print(portfolio)
+        print('Portfolio value: %.2f' % (portfolio_value))
+        print('Cash: %.2f' % (cash))
+        print('Total Value: %.2f' % (total_value))
+        print('Total Profit: %.2f' % (total_profit))
+        trade_history.to_csv('data/health.csv')
+        return trade_history,portfolio,portfolio_value,cash,total_value,total_profit
+
 if __name__ == '__main__':
     trader = Trader()
     
     start = datetime(2020,8,3).strftime('%Y-%m-%d')
-    end = datetime(2020,8,28).strftime('%Y-%m-%d')
-    tickers = client.get_collection('finance')
-    cash = 500
-
-    trader.backtest(tickers,start,end,cash)
+    end = datetime(2020,9,1).strftime('%Y-%m-%d')
+    tickers = client.get_collection('health')
+    print(len(tickers))
+    trader.simulate(tickers,start,end,1000)
