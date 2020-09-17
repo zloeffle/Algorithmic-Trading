bb3d6bf0029dfa549ad19d8b378c22cb25276bc7
zloeffle <zloeffler22@gmail.com>
2020-09-17 16:56:59 -0500

    update

--- utilities.py
+++ utilities.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import linregress
 
 ################# DESCRIPTORS #################
 def support_level(data,start,end):
